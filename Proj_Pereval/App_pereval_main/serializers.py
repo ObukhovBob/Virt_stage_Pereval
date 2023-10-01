47c5bd59a9ade53d771f2513007caf44ebe1220c
Bobilo <Bobilo@mail.ru>
2023-10-01 10:27:17 +0500

    Переделано редактирование записей. Вынесено в отдельный сериалайзер, отдельный вьювсет(PatchData).

--- Proj_Pereval/App_pereval_main/serializers.py
+++ Proj_Pereval/App_pereval_main/serializers.py
@@ -26,7 +26,53 @@ class CoordsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class PerevalSerializer(WritableNestedModelSerializer):
+class PerevalSerializer(serializers.ModelSerializer):
+    user = UsersSerializer()
+    images = ImagesSerializer()
+    coordinates = CoordsSerializer()
+
+    class Meta:
+        model = Pereval
+        fields = ['id', 'beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'status', 'level',
+                  'coordinates',
+                  'user',
+                  'images']
+
+        def create(self, validated_data, **kwargs):
+            user_data = validated_data.pop('user')
+            images_data = validated_data.pop('images')
+            coordinates_data = validated_data.pop('coordinates')
+
+            pass_user = Users.objects.filter(email=user_data['email'])
+            if pass_user.exists():
+                user_serializer = UsersSerializer(data=user_data)
+                user_serializer.is_valid(raise_exception=True)
+                user_data = user_serializer.save()
+            else:
+                user_data = Users.objects.create(**user_data)
+
+            user = Users.objects.create(**user_data)
+            images = Images.objects.create(**images_data)
+            coordinates = Coords.objects.create(**coordinates_data)
+
+            pereval = Pereval.objects.create(user=user, images=images, coordinates=coordinates, **validated_data)
+
+            for image in images:
+                image_1 = image.pop('image_1')
+                title_1 = image.pop('title_1')
+                image_2 = image.pop('image_2')
+                title_2 = image.pop('title_2')
+                image_3 = image.pop('image_3')
+                title_3 = image.pop('title_3')
+                Images.objects.create(image_1=image_1, title_1=title_1,
+                                      image_2=image_2, title_2=title_2,
+                                      image_3=image_3, title_3=title_3
+                                      )
+            pereval.save()
+            return pereval
+
+
+class PerevalNested(WritableNestedModelSerializer):
     user = UsersSerializer()
     images = ImagesSerializer()
     coordinates = CoordsSerializer()
@@ -50,38 +96,3 @@ class PerevalSerializer(WritableNestedModelSerializer):
                     or user.phone != user_data.get('phone'):
                 raise ValidationError({'message': 'Редактирование пользовательских данных запрещено'})
             return data
-
-    def create(self, validated_data, **kwargs):
-        user_data = validated_data.pop('user')
-        images_data = validated_data.pop('images')
-        coordinates_data = validated_data.pop('coordinates')
-
-        pass_user = Users.objects.filter(email=user_data['email'])
-        if pass_user.exists():
-            user_serializer = UsersSerializer(data=user_data)
-            user_serializer.is_valid(raise_exception=True)
-            user_data = user_serializer.save()
-        else:
-            user_data = Users.objects.create(**user_data)
-
-        user = Users.objects.create(**user_data)
-        images = Images.objects.create(**images_data)
-        coordinates = Coords.objects.create(**coordinates_data)
-
-        pereval = Pereval.objects.create(user=user, images=images, coordinates=coordinates, **validated_data)
-
-        for image in images:
-            image_1 = image.pop('image_1')
-            title_1 = image.pop('title_1')
-            image_2 = image.pop('image_2')
-            title_2 = image.pop('title_2')
-            image_3 = image.pop('image_3')
-            title_3 = image.pop('title_3')
-            Images.objects.create(image_1=image_1, title_1=title_1,
-                                  image_2=image_2, title_2=title_2,
-                                  image_3=image_3, title_3=title_3
-                                  )
-        pereval.save()
-        return pereval
-
-
